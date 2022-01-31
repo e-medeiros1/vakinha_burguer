86fde30630684ec2fce60dfbf835782ed4a57d38
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-31 11:30:22 -0300

    DartWeek app

---
+++ android/app/src/main/kotlin/com/example/vakinha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vakinha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vakinha Burger',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+    );
+  }
+}

7828ce1372e89b7196225bb03a5a997c89e5ee6a
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-31 22:48:23 -0300

    Widget personalizado ElevatedButton

---
+++ lib/app/core/ui/widgets/vakinha_button.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class VakinhaButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final String label;
+  final double? widht;
+  final double height;
+  final Color? color;
+
+  const VakinhaButton({
+    Key? key,
+    this.onPressed,
+    required this.label,
+    this.widht,
+    this.height = 50,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widht,
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          label,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: color,
+        ),
+      ),
+    );
+  }
+}

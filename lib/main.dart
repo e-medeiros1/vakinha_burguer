1f59cdbfb9167a6ca0addc54740679565f55a327
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-31 22:51:51 -0300

    Aplicando rotas e o tema no main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha/app/core/ui/vakinha_ui.dart';
+import 'package:vakinha/app/routes/auth_routes.dart';
+import 'package:vakinha/app/routes/splash_routes.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const VakinhaBurguerMainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class VakinhaBurguerMainApp extends StatelessWidget {
+  const VakinhaBurguerMainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      //Para tirar o banner de debug
       debugShowCheckedModeBanner: false,
       title: 'Vakinha Burger',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      //Adicionando o tema
+      theme: VakinhaUI.theme,
+      //Trabalhando com rotas no Getx
+      getPages: [
+        ...SplashRoutes.routers,
+        ...AuthRoutes.routers,
+        
+      ],
     );
   }
 }

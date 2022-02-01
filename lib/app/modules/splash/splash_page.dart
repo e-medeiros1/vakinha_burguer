058620c48b528d80d65b1f4a297269f3d7261510
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-01-31 22:49:00 -0300

    Splash da aplicação

---
+++ lib/app/modules/splash/splash_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha/app/core/ui/widgets/vakinha_button.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color(0xFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.width,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.heightTransformer(
+                      reducedBy: 85,
+                    ),
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  VakinhaButton(
+                    label: 'ACESSAR',
+                    onPressed: () {
+                      Get.toNamed('/auth/login');
+                    },
+                    widht: context.widthTransformer(reducedBy: 40),
+                    height: 40,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

b96ee8e2ee40ca8e533cfa97e1ba2be4f773b2ae
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-02 17:21:59 -0300

    Dispose, controller e validate de login e register

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,78 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:vakinha/app/core/ui/vakinha_state.dart';
 import 'package:vakinha/app/core/ui/vakinha_ui.dart';
 import 'package:vakinha/app/core/ui/widgets/vakinha_appbar.dart';
 import 'package:vakinha/app/core/ui/widgets/vakinha_button.dart';
-import 'package:vakinha/app/core/ui/widgets/vakinha_textFormField.dart';
+import 'package:vakinha/app/core/ui/widgets/vakinha_textformfield.dart';
+import 'package:vakinha/app/modules/auth/login/login_controller.dart';
+import 'package:validatorless/validatorless.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends VakinhaState<LoginPage, LoginController> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailEC.dispose();
+    _passwordEC.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: VakinhaAppBar(
         elevation: 0,
       ),
-      body: LayoutBuilder(builder: (_, constraints) {
-        return ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: constraints.maxHeight,
-          ),
-          child: IntrinsicHeight(
-              child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Login',
-                    style: context.textTheme.headline6?.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: context.theme.primaryColorDark),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const VakinhaTextFormField(label: 'E-mail'),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const VakinhaTextFormField(label: 'Senha'),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Center(
-                    child: VakinhaButton(
-                      label: 'ENTRAR',
-                      onPressed: () {},
-                      widht: context.width,
+      backgroundColor: Colors.white,
+      body: LayoutBuilder(
+        builder: (_, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Login',
+                          style: context.textTheme.headline6?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: context.theme.primaryColorDark),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        VakinhaTextFormField(
+                          label: 'E-mail',
+                          controller: _emailEC,
+                          validator: Validatorless.multiple([
+                            Validatorless.required('E-mail obrigatório'),
+                            Validatorless.email('E-mail inválido'),
+                          ]),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        VakinhaTextFormField(
+                          label: 'Senha',
+                          obscureText: true,
+                          controller: _passwordEC,
+                          validator: Validatorless.multiple([
+                            Validatorless.required('Senha obrigatório'),
+                            Validatorless.min(
+                                6, 'Senha deve conter pelo menos 6 caracteres'),
+                          ]),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Center(
+                          child: VakinhaButton(
+                            widht: double.infinity,
+                            label: 'ENTRAR',
+                            onPressed: () {
+                              final formValid =
+                                  _formKey.currentState?.validate() ?? false;
+                              if (formValid) {
+                                controller.login(
+                                  email: _emailEC.text,
+                                  password: _passwordEC.text,
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                        const Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Não possui uma conta?'),
+                            TextButton(
+                              onPressed: () {
+                                Get.toNamed('/auth/register');
+                              },
+                              child: const Text(
+                                'Cadastre-se',
+                                style: VakinhaUI.textBold,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  const Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Não possui uma conta?'),
-                      TextButton(
-                        onPressed: () {
-                          Get.offNamed('/auth/register');
-                        },
-                        child: const Text(
-                          'Cadastre-se',
-                          style: VakinhaUI.textBold,
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                ),
               ),
             ),
-          )),
-        );
-      }),
+          );
+        },
+      ),
     );
   }
 }
--- lib/app/modules/auth/register/register.page.dart
+++ lib/app/modules/auth/register/register.page.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:vakinha/app/core/ui/vakinha_state.dart';
 import 'package:vakinha/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:get/get.dart';
 import 'package:vakinha/app/core/ui/widgets/vakinha_button.dart';
-import 'package:vakinha/app/core/ui/widgets/vakinha_textFormField.dart';
+import 'package:vakinha/app/core/ui/widgets/vakinha_textformfield.dart';
+import 'package:vakinha/app/modules/auth/register/register_controller.dart';
+import 'package:validatorless/validatorless.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState
+    extends VakinhaState<RegisterPage, RegisterController> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameEC = TextEditingController();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+
+
+  @override
+  void dispose() {
+    _nameEC.dispose();
+    _emailEC.dispose();
+    _passwordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: VakinhaAppBar(
         elevation: 0,
       ),
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: LayoutBuilder(builder: (_, constraints) {
-          return IntrinsicHeight(
-              child: Padding(
+        child: IntrinsicHeight(
+          child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Form(
+              key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -36,34 +59,72 @@ class RegisterPage extends StatelessWidget {
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextFormField(label: 'Nome*'),
+                  VakinhaTextFormField(
+                    label: 'Nome',
+                    controller: _nameEC,
+                    validator: Validatorless.required('Nome Obrigatório'),
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextFormField(label: 'E-mail*'),
+                  VakinhaTextFormField(
+                    label: 'E-mail',
+                    controller: _emailEC,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('E-mail obrigatório'),
+                      Validatorless.email('E-mail inválido')
+                    ]),
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextFormField(label: 'Senha*'),
+                  VakinhaTextFormField(
+                    label: 'Senha',
+                    controller: _passwordEC,
+                    obscureText: true,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Senha obrigatório'),
+                      Validatorless.min(
+                          6, 'Senha deve conter pelo menos 6 caracteres'),
+                    ]),
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextFormField(label: 'Confirmar senha*'),
+                  VakinhaTextFormField(
+                    label: 'Confirma senha',
+                    obscureText: true,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Confirma senha obrigatória'),
+                      Validatorless.compare(
+                          _passwordEC, 'Senha diferente de confirma senha'),
+                    ]),
+                  ),
                   const SizedBox(
-                    height: 50,
+                    height: 30,
                   ),
                   Center(
                     child: VakinhaButton(
-                      label: 'CADASTRAR',
-                      onPressed: () {},
-                      widht: context.width,
+                      widht: double.infinity,
+                      label: 'Cadastrar',
+                      onPressed: () {
+                        final formValid =
+                            _formKey.currentState?.validate() ?? false;
+                        if (formValid) {
+                          controller.register(
+                            name: _nameEC.text,
+                            email: _emailEC.text,
+                            password: _passwordEC.text,
+                          );
+                        }
+                      },
                     ),
                   ),
                 ],
               ),
             ),
-          ));
-        }),
+          ),
+        ),
       ),
     );
   }

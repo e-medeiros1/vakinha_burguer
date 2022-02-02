ff1c9edded7e37ea575e980e0525e9725dedc33c
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-02 17:23:08 -0300

    Get para checar se user tá logado

--- lib/app/modules/splash/splash_controller.dart
+++ lib/app/modules/splash/splash_controller.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import 'package:vakinha/app/core/services/auth_service.dart';
 
-class SplashController extends GetxController { 
-//Uma page não pode fazer ações diretamente nela tem que chamar uma 
+class SplashController extends GetxController {
+  void checkLogged() {
+    //Adicionando no get o AuthService e inicializando ele
+    Get.putAsync(() => AuthService().init());
+  }
+//Uma page não pode fazer ações diretamente nela tem que chamar uma
 // controller e um controller é alimentada por um binding
-
-
-
 
 }

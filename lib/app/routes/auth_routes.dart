321a378c0a54dd71712841d89f063287c2665b61
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-01 23:01:56 -0300

    Adicionando os Bindings da rota e login

--- lib/app/routes/auth_routes.dart
+++ lib/app/routes/auth_routes.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:vakinha/app/modules/auth/login/login_bindings.dart';
 import 'package:vakinha/app/modules/auth/login/login_page.dart';
 import 'package:vakinha/app/modules/auth/register/register.page.dart';
+import 'package:vakinha/app/modules/auth/register/register_bindings.dart';
 
 class AuthRoutes {
   AuthRoutes._();
@@ -8,10 +10,12 @@ class AuthRoutes {
   static final routers = <GetPage>[
     GetPage(
       name: '/auth/login',
+      binding: LoginBindings(),
       page: () => const LoginPage(),
     ),
     GetPage(
       name: '/auth/register',
+      binding: RegisterBindings(),
       page: () => const RegisterPage(),
     ),
   ];

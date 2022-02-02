44625e8bbfcd6ba09ecf709fb2611ec7e3c05ea6
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-02 18:06:19 -0300

    Adcionando um botão de logout

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,16 +1,27 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:vakinha/app/core/services/auth_service.dart';
 import './home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
-    
-    const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('HomePage'),),
-            body: Container(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: Center(
+          child: TextButton(
+        onPressed: () {
+          Get.find<AuthService>().logout();
+        },
+        child: const Text(
+          'Logout',
+          style: TextStyle(fontSize: 20),
+        ),
+      )),
+    );
+  }
 }

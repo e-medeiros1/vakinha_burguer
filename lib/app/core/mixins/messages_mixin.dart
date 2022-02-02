73eeed19a9b241f7545eb446661781d8179752d4
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-01 23:07:55 -0300

    Mixin para mensagem de erro e informações

---
+++ lib/app/core/mixins/messages_mixin.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+mixin MessagesMixin on GetxController {
+  void messageListener(Rxn<MessageModel> message) {
+    ever<MessageModel?>(message, (model) async {
+      if (model != null) {
+        Get.snackbar(
+          model.title,
+          model.message,
+          backgroundColor: model.type.color(),
+          colorText: model.type.textColor(),
+          margin: const EdgeInsets.all(20),
+        );
+      }
+    });
+  }
+}
+
+class MessageModel {
+  final String title;
+  final String message;
+  final MessageType type;
+
+  MessageModel({
+    required this.title,
+    required this.message,
+    required this.type,
+  });
+}
+
+enum MessageType { error, info }
+
+extension MessageTypeColorExt on MessageType {
+  Color color() {
+    switch (this) {
+      case MessageType.error:
+        return Colors.red[800]!;
+      case MessageType.info:
+        return Colors.blue[200]!;
+    }
+  }
+
+  Color textColor() {
+    switch (this) {
+      case MessageType.error:
+        return Colors.white;
+      case MessageType.info:
+        return Colors.black;
+    }
+  }
+}

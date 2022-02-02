d7859c104fbb7dcbd816686971bfc3bcf41cd9fd
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-02 15:53:56 -0300

    Saindo de Stateless e indo para GetView

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vakinha/app/core/ui/widgets/vakinha_button.dart';
+import 'package:vakinha/app/modules/splash/splash_controller.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends GetView<SplashController> {
   const SplashPage({Key? key}) : super(key: key);
 
   @override

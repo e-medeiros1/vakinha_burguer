ba40e9f547e5f32a87da7cbb7ef87b03d5f87633
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-03 16:26:39 -0300

    Criando widgetpersonalizado de plus e minus

---
+++ lib/app/core/ui/widgets/vakinha_rounded_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class VakinhaRoundedButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String label;
+  final double fontSize;
+
+  const VakinhaRoundedButton({
+    Key? key,
+    required this.onPressed,
+    required this.label,
+    this.fontSize = 25,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: TextStyle(color: Colors.grey, fontSize: fontSize),
+      ),
+      style: ElevatedButton.styleFrom(
+        shape: const CircleBorder(),
+        primary: Colors.white,
+      ),
+    );
+  }
+}

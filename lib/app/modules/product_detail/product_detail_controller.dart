1dee86b23ba9a4dbae14a45712445631ba6a845f
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-03 16:53:44 -0300

    Implementando métodos no controller

--- lib/app/modules/product_detail/product_detail_controller.dart
+++ lib/app/modules/product_detail/product_detail_controller.dart
@@ -1,3 +1,37 @@
 import 'package:get/get.dart';
+import 'package:vakinha/app/models/product_model.dart';
 
-class ProductDetailController extends GetxController {}
+class ProductDetailController extends GetxController {
+  final _product = Rx<ProductModel>(Get.arguments);
+  final _totalPrice = 0.0.obs;
+  final _quantity = 1.obs;
+
+  ProductModel get product => _product.value;
+  double get totalPrice => _totalPrice.value;
+  int get quantity => _quantity.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _totalPrice(product.price);
+
+    ever<int>(_quantity, (quantity) {
+      _totalPrice(product.price * quantity);
+    });
+
+    //Reatividade: Worker que toda vez que _quantity for alterado,
+    //será feito uma atualização do _totalPrice
+  }
+
+  void addProduct() {
+    _quantity.value += 1;
+  }
+
+  void removeProduct() {
+    if (_quantity > 1) {
+      _quantity.value -= 1;
+    }
+  }
+}
+
+//Faz com que os products estejam disponíveis

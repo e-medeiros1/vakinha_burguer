99a05ab30542b14bc3ce04c11ba0e0ee0cf3c227
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-03 18:34:22 -0300

    Adicionando novo service para carrinho de compras

--- lib/app/core/bindings/application_binding.dart
+++ lib/app/core/bindings/application_binding.dart
@@ -1,15 +1,14 @@
 import 'package:get/get.dart';
 import 'package:vakinha/app/core/rest_client/rest_client.dart';
+import 'package:vakinha/app/core/services/shopping_card_service.dart';
 
 class ApplicationBinding implements Bindings {
   @override
   void dependencies() {
-    //Aqui será colocado tudo o que quero que seja utilizado pela aplicação toda
-
     Get.lazyPut(
       () => RestClient(),
       fenix: true,
     );
-    //Serve pra tudo que quer manter sempre ativo durante a aplicação como um todo
+    Get.lazyPut<dynamic>(() => ShoppingCardService());
   }
 }

c0845a971a09c7e55f9709be13b5868f9595417e
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-03 18:34:37 -0300

    Service carrinho de compra

---
+++ lib/app/core/services/shopping_card_service.dart
@@ -0,0 +1,32 @@
+import 'package:get/get.dart';
+import 'package:vakinha/app/models/product_model.dart';
+import 'package:vakinha/app/models/shopping_card_model.dart';
+
+class ShoppingCardService extends GetxService {
+  final _shoppingCard = <int, ShoppingCardModel>{}.obs;
+
+  List<ShoppingCardModel> get products => _shoppingCard.values.toList();
+
+  int get totalProducts => _shoppingCard.values.length;
+
+  ShoppingCardModel? getById(int id) => _shoppingCard[id];
+
+  void addAndRemoveProductInShoppingCard(
+    ProductModel product, {
+    required int quantity,
+  }) {
+    if (quantity == 0) {
+      _shoppingCard.remove(product.id);
+    } else {
+      _shoppingCard.update(product.id, (product) {
+        product.quantity == quantity;
+        return product;
+      }, ifAbsent: () {
+        return ShoppingCardModel(
+          quantity: quantity,
+          product: product,
+        );
+      });
+    }
+  }
+}

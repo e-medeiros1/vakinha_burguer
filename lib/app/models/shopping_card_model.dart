1c733b8cc710cbb849da08f89e380c920b78db3f
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-03 18:34:59 -0300

    Novo modelo para carrinho de compra

---
+++ lib/app/models/shopping_card_model.dart
@@ -0,0 +1,12 @@
+import 'package:vakinha/app/models/product_model.dart';
+
+class ShoppingCardModel {
+  int quantity;
+  ProductModel product;
+  
+
+  ShoppingCardModel({
+    required this.quantity,
+    required this.product,
+  });
+}
